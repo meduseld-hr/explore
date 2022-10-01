fdf2266b50499f802d2d655d19397464cfc1aab3
Joseph <josephccchou@gmail.com>
2022-10-01 16:04:22 -0400

    set up socket io connection between dashboard route and server

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { UserContext } from "../contexts/user";
-
+import Chat from '../components/dashboard/Chat.jsx';
 
 export default function Dashboard () {
   const user = useContext(UserContext);
   return (
     <div>
       Dashboard is here for {user && user.name}
+      <Chat />
     </div>
   )
 }

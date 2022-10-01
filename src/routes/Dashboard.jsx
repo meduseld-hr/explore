af264b4c29926e36be9a753228675f92b4084ef3
Brian <itscrousie@gmail.com>
2022-10-01 14:47:37 -0400

    Implement Sidebar and Staging Area to Dashboard

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,12 +1,23 @@
 import { useContext } from "react";
+import styled from "styled-components";
 import { UserContext } from "../contexts/user";
-
+import SideBar from "../components/dashboard/Sidebar.jsx"
+import StagingArea from "../components/dashboard/StagingArea.jsx"
 
 export default function Dashboard () {
   const user = useContext(UserContext);
   return (
-    <div>
-      Dashboard is here for {user && user.name}
-    </div>
+    <DashContainer>
+      <SideBar/>
+      <StagingArea/>
+    </DashContainer>
   )
 }
+
+const DashContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  border: 2px solid black;
+`

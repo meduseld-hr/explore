ec5dc339a36d99b1813ae14f4a4e5ce84e1dfec2
Nick Adam <na4hx@virginia.edu>
2022-10-01 12:24:07 -0400

    adds updated schema file

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -9,8 +9,10 @@ CREATE TABLE users (
   PRIMARY KEY(id)
 );
 
+CREATE INDEX users_id_idx ON users (id);
+
 CREATE TABLE trips (
-  id INT SERIAL,
+  id SERIAL,
   trip_name VARCHAR(1000) NOT NULL,
   origin_google_place_id VARCHAR(10000),
   thumbnail_url VARCHAR(10000),
@@ -22,8 +24,8 @@ CREATE TABLE trips (
 );
 
 CREATE TABLE stops (
-  id INT SERIAL,
-  order INT,
+  id SERIAL,
+  stop_order INT,
   stop_name VARCHAR(1000),
   trip_id INT,
   thumbnail_url VARCHAR(10000),
@@ -35,8 +37,10 @@ CREATE TABLE stops (
   FOREIGN KEY(trip_id) REFERENCES trips(id)
 );
 
+CREATE INDEX stops_trip_id_idx ON stops (trip_id);
+
 CREATE TABLE trips_users (
-  id INT SERIAL,
+  id SERIAL,
   trip_id INT,
   user_id VARCHAR(10000),
   trip_owner BOOLEAN,
@@ -48,8 +52,11 @@ CREATE TABLE trips_users (
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE INDEX trips_users_trip_id_idx ON trips_users (trip_id);
+CREATE INDEX trips_users_user_id_idx ON trips_users (user_id);
+
 CREATE TABLE messages (
-  id INT SERIAL,
+  id SERIAL,
   body VARCHAR(10000),
   trip_id INT,
   user_id VARCHAR(10000),
@@ -60,8 +67,10 @@ CREATE TABLE messages (
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE INDEX messages_trip_id_idx ON messages (trip_id);
+
 CREATE TABLE comments (
-  id INT SERIAL,
+  id SERIAL,
   body VARCHAR(10000),
   trip_id INT,
   user_id VARCHAR(10000),
@@ -72,3 +81,5 @@ CREATE TABLE comments (
   FOREIGN KEY(trip_id) REFERENCES trips(id),
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+CREATE INDEX comments_trip_id_idx ON comments (trip_id);

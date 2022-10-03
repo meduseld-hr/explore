8331f66d78416848baacb1224370ddcec09013af
Daniel Elliott <daniel.s.elliott.33@gmail.com>
2022-10-03 16:49:57 -0500

    burn it down part 2, markers show onclick

--- src/components/dashboard/Expanded-Map/Map.jsx
+++ src/components/dashboard/Expanded-Map/Map.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   GoogleMap,
   useLoadScript,
+  Marker,
 } from "@react-google-maps/api";
 
 const MAPS_SECRET = "AIzaSyBWNNF-l95ID334274nOsP0JdPa79H96BA";
 
-const libraries = ["places"];
+// const libraries = ["places"];
 
 export default function App() {
+  //setting libraries variable so that console doesn't give warning anymore, per stackOverflow
+  const [libraries] = useState(['places']);
   const {isLoaded, loadError} = useLoadScript({
     googleMapsApiKey: MAPS_SECRET,
     libraries,
   });
+  const [markers, setMarkers] = useState([]);
 
   if(loadError) {
     return "Error loading maps";
+  }
+  if(!isLoaded) {
+    return "Loading Maps";
   }
 
   const center = {
@@ -25,9 +32,8 @@ export default function App() {
 
   const mapOptions = {
     disableDefaultUI: false,
-    zoomControl: false,
+    zoomControl: true,
   }
-
 
   return <div>
     <GoogleMap
@@ -35,7 +41,21 @@ export default function App() {
       zoom={8}
       center={center}
       options={mapOptions}
+      onClick={(e) => {
+        console.log(e);
+        setMarkers(prev => [...prev,{
+          lat: e.latLng.lat(),
+          lng: e.latLng.lng(),
+          time: new Date(),
+        }]);
+      }}
     >
+      {markers.map((marker, index) => (
+        <Marker
+          key={marker.time.toISOString()}
+          position={{lat: marker.lat, lng: marker.lng}}
+        />
+      ))}
     </GoogleMap>
   </div>;
 

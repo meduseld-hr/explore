fa45383feb0fd0400885cb3e3938a66a6e04f333
Jeremy-Biagiotti <Jbiagiotti8@gmail.com>
2022-10-05 16:09:49 -0400

    edidted css to display correctly

--- src/components/dashboard/Post-Trip/CommentContainer.jsx
+++ src/components/dashboard/Post-Trip/CommentContainer.jsx
@@ -1,46 +1,50 @@
-import React, {useEffect, useState, useContext} from "react";
+import React, { useEffect, useState, useContext } from "react";
 import styled from "styled-components";
-import api from '../../../functions/api';
-import {UserContext} from '../../../contexts/user';
-import {useParams} from "react-router-dom";
+import api from "../../../functions/api";
+import { UserContext } from "../../../contexts/user";
+import { useParams } from "react-router-dom";
 
 const CommentContainer = () => {
-
   const user = useContext(UserContext);
-  const {tripId} = useParams();
+  const { tripId } = useParams();
 
   const [comments, setComments] = useState([]);
-  const [body, setBody] = useState('');
+  const [body, setBody] = useState("");
 
   useEffect(() => {
-    api.get(`/dashboard/${tripId}`)
+    api
+      .get(`/dashboard/${tripId}`)
       .then((response) => {
         setComments(response.data[2]);
       })
       .catch((err) => {
         console.log(err);
-      })
-  }, [])
+      });
+  }, []);
 
   return (
     <CommentGrid>
       {comments.map((comment, index) => (
-          <div key={index}>
-            <img src={comment.picture} />
-            <strong>{comment.nickname}</strong>
-            <div>{comment.body}</div>
-          </div>
-        ))}
-      <button onClick={() => {
-        api.post(`/dashboard/${tripId}/comment`, {body, timeStamp: Date.now()})
-          .catch((err) => {
-            console.log(err);
-          });
-      }}></button>
-
+        <div key={index}>
+          <img src={comment.picture} />
+          <strong>{comment.nickname}</strong>
+          <div>{comment.body}</div>
+        </div>
+      ))}
+      <button
+        onClick={() => {
+          api
+            .post(`/dashboard/${tripId}/comment`, {
+              body,
+              timeStamp: Date.now(),
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }}
+      ></button>
     </CommentGrid>
   );
-
 };
 
 const CommentGrid = styled.div`
@@ -48,7 +52,6 @@ const CommentGrid = styled.div`
   width: 100%;
   height: 100%;
   max-height: 100%;
-  display: flex;
   border: solid;
 `;
 

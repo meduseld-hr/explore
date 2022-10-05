b611e1fc571a7c0bdd3b9b8ec38e95b620dc128f
Daniel Elliott <daniel.s.elliott.33@gmail.com>
2022-10-05 09:10:35 -0500

    hardcoded directions render and center on map with only origin/destination, no waypoints

--- src/components/dashboard/Expanded-Map/Map.jsx
+++ src/components/dashboard/Expanded-Map/Map.jsx
@@ -5,7 +5,9 @@ import {
   Marker,
   InfoWindow,
   Autocomplete,
-  DirectionsRenderer
+  DirectionsService,
+  DirectionsRenderer,
+  Polyline,
 } from "@react-google-maps/api";
 import { Loader } from '@googlemaps/js-api-loader';
 import usePlacesAutocomplete, {
@@ -31,14 +33,42 @@ export default function App() {
     lat: 30.27466235839214,
     lng: -97.74035019783334,
   })
+
   const [autocomplete, setAutoComplete] = useState('');
   const [markers, setMarkers] = useState([]);
   const [selected, setSelected] = useState(null);
   const [locationSearch, setLocationSearch] = useState('');
-  const originRef = useRef();
-  const destinationRef = useRef();
+
   const searchRef = useRef(null);
   const mapRef = useRef();
+
+
+  const [map, setMap] = useState(null);
+  const [tripRoute, setTripRoute] = useState(null)
+  // const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState('');
+  const [duration, setDuration] = useState('');
+  const originRef = useRef();
+  const destinationRef = useRef();
+
+  // async function calculateRoute () {
+  //   const directionsService = new google.maps.DirectionsService()
+  //   const results = await directionsService.route({
+  //     origin: "Austin, TX",
+  //     destination: "Bee Cave, TX",
+  //     travelMode: google.maps.TravelMode.DRIVING
+  //   })
+  //   setDirectionsResponse(results);
+  //   setDistance(results.routes[0].legs[0].distance.value);
+  //   setDuration(results.routes[0].legs[0].duration.value);
+
+  // }
+  function clearDirections() {
+    setTripRoute(null);
+    setDistance('');
+    setDuration('');
+    //reset origin and destination
+  }
 
   if(loadError) {
     return "Error loading maps";
@@ -63,10 +93,10 @@ export default function App() {
       });
     })
   }
-  const throttleIdle = _.debounce(handleIdle, 1000)
 
-  const onLoad = (autocomplete) => {
-    console.log('autocomplete: ', autocomplete);
+  const throttleIdle = _.debounce(handleIdle, 1000);
+
+  const onSearchLoad = (autocomplete) => {
     setAutoComplete(autocomplete);
   }
 
@@ -83,15 +113,50 @@ export default function App() {
     }
   }
 
+  const testDirections = {
+    lat: 30.37466235839214,
+    lng: -97.84035019783334,
+  }
+
+  const handleDirections = (origin, destination) => {
+    const loader = new Loader({apiKey:MAPS_SECRET});
+    loader.load().then(() => {
+    const directionsService = new google.maps.DirectionsService();
+    directionsService.route({
+      origin:  "Austin, Texas",
+      destination:  "Bee Cave, Texas",
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (directions) => {
+      setTripRoute(directions);
+      console.log("directions set")
+      }
+    )})
+    .catch(err => {
+      console.log(err)
+    });
+  }
+
+
+  const tripPath = [
+    {lat: 30.281812053292956, lng: -97.73815329458314},
+    {lat: 30.230781421835704, lng: -97.75279748278045},
+    {lat: 30.26518019321872, lng: -97.7720555943571},
+    {lat: 30.301887158191406, lng: -97.8260877885794}
+  ]
+
   return <div>
     <GoogleMap
       mapContainerClassName="map-container"
       mapContainerStyle={mapContainerStyle}
       onCenterChanged={throttleIdle}
+      onClick={handleDirections}
       zoom={8}
       center={center}
       ref={mapRef}
       options={mapOptions}
+      onLoad={(map) => {
+        setMap(map);
+      }}
     >
 
       {markers.map((marker, index) => (
@@ -101,14 +166,16 @@ export default function App() {
         ><Info marker={marker}/></InfoWindow>
       ))}
 
+      {tripRoute && <DirectionsRenderer directions={tripRoute} />}
+
       <Autocomplete
-        onLoad={onLoad}
+        onLoad={onSearchLoad}
         onPlaceChanged={onPlaceChanged}
       >
         <div>
           <SearchInput
             type="text"
-            placeholder="Find a location:"
+            placeholder="Go to a destination:"
             ref={searchRef}
           />
         </div>

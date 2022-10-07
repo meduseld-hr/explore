9a37f234a437dba7d245530b9b6d75dcd8c079d0
Brian <itscrousie@gmail.com>
2022-10-07 14:01:02 -0400

    Style buttons on trips/detail

--- src/components/dashboard/Chat.jsx
+++ src/components/dashboard/Chat.jsx
@@ -199,9 +199,9 @@ const AddMessageWrapper = styled.div`
 const Button = styled.button`
   width: 15%;
   margin: auto;
-  color: ${(props) => props.theme.color}
-  background-color: ${(props) => props.theme.background};
   border-radius: 12px;
+  color: ${(props) => props.theme.buttonColor};
+  background-color: ${(props) => props.theme.button};
   cursor: pointer;
 `;
 
